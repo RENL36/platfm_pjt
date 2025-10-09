7c462577c85969b9493ea85966f21cc4ea575206
Xéronos <rentylucie@gmail.com>
2025-10-09 09:16:19 +0200

    Update install-ansible.sh

--- vagrant/install-ansible.sh
+++ vagrant/install-ansible.sh
@@ -1,7 +1,7 @@
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y software-properties-common gnupg
+apt-get install -y software-properties-common gnupg
 
-sudo add-apt-repository --yes --update ppa:ansible/ansible
+add-apt-repository --yes --update ppa:ansible/ansible
 
 
