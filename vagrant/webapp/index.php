6dd1be4eabaf7b6d75e493e423e1632e716c5f69
Mikaël <mikael.ferreira0203@gmail.com>
2025-10-14 00:18:40 +0200

    Connexion extérieur -> docker

--- vagrant/webapp/index.php
+++ vagrant/webapp/index.php
@@ -5,7 +5,7 @@
     <title>Service déploiement de containers</title>
 </head>
 <body>
-    <h1>Connexion</h1>
+    <h1>Inscription</h1>
     <p><form action="index.php" method="post">
   <ul>
     <li>
@@ -28,7 +28,7 @@ $db_server = "localhost";
 $db_user = "root";
 $db_pass = "azerty";
 $db_name = "manage_clients_db";
-$conn = new mysqli($db_server, $db_user, $db_pass, $db_name);
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 if ($conn) {
     echo "Connecté";
 }
@@ -36,8 +36,15 @@ else {
     echo "Pas connecté";
 }
 
-
+$sql = "INSERT INTO users(name, ip_client) VALUES ('".$_POST['user_name']."', '".$_SERVER['REMOTE_ADDR']."')";
+$insert = mysqli_query($conn, $sql);
+$port = rand(10000, 60000);
 }
+$sql = "INSERT INTO container_client(container_port, id_client) VALUES ($port, LAST_INSERT_ID())";
+$insert = mysqli_query($conn, $sql);
+
+$conn->close();
+
 
 
 ?>
