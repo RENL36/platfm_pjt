48241b598418bd0bafacc765a0e7657fabd1a236
Xéronos <rentylucie@gmail.com>
2025-12-24 10:37:02 +0100

    Update supp_container.sh

--- vagrant/supp_container.sh
+++ vagrant/supp_container.sh
@@ -25,8 +25,11 @@ if docker rm -f "$CONTAINER_NAME"; then
     DB_NAME="base"
 
     # Récupérer le contrat_id avant suppression
-    SQL1="SELECT contrat_id FROM containers WHERE container_name='${CONTAINER_NAME}';"
-    CONTRAT_ID=$(mysql -N -u "$DB_USER" -p"$DB_PASS" -D "$DB_NAME" -e "$SQL1")
+    SQL1="SELECT * FROM containers WHERE container_name='${CONTAINER_NAME}';"
+    TUPLE=$(mysql -N -u "$DB_USER" -p"$DB_PASS" -D "$DB_NAME" -e "$SQL1")
+    CONTRAT_ID=TUPLE[3]
+    PORT=TUPLE[2]
+
 
     if CONTRAT_ID == null; then
       echo "Le conteneur supprimé n'appartient à personne"
@@ -37,13 +40,15 @@ if docker rm -f "$CONTAINER_NAME"; then
       SQL2="SELECT * from containers where contrat_id = NULL;"
       TABLE_CONTAINERS=$(mysql -N -u "$DB_USER" -p"$DB_PASS" -D "$DB_NAME" -e "$SQL2")
       NEW_CONTAINER=TABLE_CONTAINERS[0]
-      SQL3="UPDATE containers SET container_id=NEW_CONTAINER[0], container_name=[1], container_port=[2], container_contrat=CONTRAT_ID"
+      SQL3="UPDATE containers set container_id=NEW_CONTAINER[0], container_name=NEW_CONTAINER[1], container_port=PORT, container_contrat=CONTRAT_ID where "
       mysql -u "$DB_USER" -p"$DB_PASS" -D "$DB_NAME" -e "$SQL3"
-      echo "Nouveau conteneur attribué" 
-      
+      SQL4=""
+      echo "Nouveau conteneur attribué" + 
+      docker exec -u root $container bash -c \"id $user_esc >/dev/null 2>&1 || (useradd -m -s /bin/bash $user_esc && echo '$user_esc:$pass_esc' | chpasswd)
+   
     # Supprimer la ligne du conteneur
-    SQL4="DELETE FROM containers WHERE container_name='${CONTAINER_NAME}';"
-    mysql -u "$DB_USER" -p"$DB_PASS" -D "$DB_NAME" -e "$SQL2"
+    SQL5="DELETE FROM containers WHERE container_name='${CONTAINER_NAME}';"
+    mysql -u "$DB_USER" -p"$DB_PASS" -D "$DB_NAME" -e "$SQL5"
 
     if [ $? -eq 0 ]; then
         echo "Ligne du conteneur supprimée dans la table 'containers'."
